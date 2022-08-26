40fc0cd4626831007f9a88cad06d9aacd12a3d02
Harriet Sands <harrietrsands@gmail.com>
2022-08-26 18:06:44 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,17 +3,28 @@
 set -e
 set -x
 
-if [ -z "$INPUT_SOURCE_DIRECTORY" ]
+if [ -z "$INPUT_SOURCE_FOLDER" ]
 then
-  echo "Source directory must be defined"
+  echo "Source folder must be defined"
   return -1
 fi
 
-echo "Setting git variables"
-CLONE_DIR=$(mktemp -d)
-TIME_ID=$(date +%s)
-DESTINATION_HEAD_BRANCH=$INPUT_DESTINATION_HEAD_BRANCH_PREFIX$TIME_ID
+if [ $INPUT_DESTINATION_HEAD_BRANCH == "main" ] || [ $INPUT_DESTINATION_HEAD_BRANCH == "master"]
+then
+  echo "Destination head branch cannot be 'main' nor 'master'"
+  return -1
+fi
 
+if [ -z "$INPUT_PULL_REQUEST_REVIEWERS" ]
+then
+  PULL_REQUEST_REVIEWERS=$INPUT_PULL_REQUEST_REVIEWERS
+else
+  PULL_REQUEST_REVIEWERS='-r '$INPUT_PULL_REQUEST_REVIEWERS
+fi
+
+CLONE_DIR=$(mktemp -d)
+
+echo "Setting git variables"
 export GITHUB_TOKEN=$API_TOKEN_GITHUB
 git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$INPUT_USER_NAME"
@@ -22,35 +33,24 @@ echo "Cloning destination git repository"
 git clone "https://$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 
 echo "Copying contents to git repo"
-mkdir -p $CLONE_DIR/$INPUT_DESTINATION_DIRECTORY/
-cp -a $INPUT_SOURCE_DIRECTORY "$CLONE_DIR/"
+mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER/
+cp $INPUT_SOURCE_FOLDER "$CLONE_DIR/$INPUT_DESTINATION_FOLDER/"
 cd "$CLONE_DIR"
-git config --global --add safe.directory "$CLONE_DIR"
-git checkout -b "$DESTINATION_HEAD_BRANCH"
+git checkout -b "$INPUT_DESTINATION_HEAD_BRANCH"
 
 echo "Adding git commit"
 git add .
-
-if [ -z "$INPUT_FILES_TO_REMOVE_PATH" ]
-then
-  echo "Including all files in PR"
-else
-  cat $INPUT_FILES_TO_REMOVE_PATH | xargs git rm -rf --cached
-fi
-
 if git status | grep -q "Changes to be committed"
 then
-  git commit --message "$INPUT_COMMIT_MESSAGE"
-  
+  git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
   echo "Pushing git commit"
-  git push -u origin HEAD:$DESTINATION_HEAD_BRANCH
-
+  git push -u origin HEAD:$INPUT_DESTINATION_HEAD_BRANCH
   echo "Creating a pull request"
-  gh pr create -t $DESTINATION_HEAD_BRANCH \
-             -b $DESTINATION_HEAD_BRANCH \
-             -B $INPUT_DESTINATION_BASE_BRANCH \
-             -H $DESTINATION_HEAD_BRANCH \
-                $PULL_REQUEST_REVIEWERS
+  gh pr create -t $INPUT_DESTINATION_HEAD_BRANCH \
+               -b $INPUT_DESTINATION_HEAD_BRANCH \
+               -B $INPUT_DESTINATION_BASE_BRANCH \
+               -H $INPUT_DESTINATION_HEAD_BRANCH \
+                  $PULL_REQUEST_REVIEWERS
 else
   echo "No changes detected"
 fi
