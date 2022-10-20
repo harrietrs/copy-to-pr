db82144ea93709f66c05aa0e572daedad5c8a9c3
Harriet Sands <harriet.sands1@nhs.net>
2022-10-20 16:54:26 +0100

    test without git rm

--- entrypoint.sh
+++ entrypoint.sh
@@ -42,8 +42,6 @@ git add .
 if [ -z "$INPUT_FILES_TO_REMOVE_PATH" ]
 then
   echo "Including all files in PR"
-else
-  cat $INPUT_FILES_TO_REMOVE_PATH | xargs git rm -rf 
 fi
 
 if git status | grep -q "Changes to be committed"
